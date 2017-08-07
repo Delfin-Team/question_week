0027c0184cf0b37083cebf02e02193d2c1f472cf
gil <blackdem17@hotmail.com>
2017-08-07 18:51:59 -0500

    :clipboard: yield added to extra css and js added to use the menu button for mobile devices

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Question Week</title>
@@ -12,7 +12,7 @@
 
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+     @yield('extra-css')
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -34,5 +34,11 @@
     <!-- Latest compiled and minified JavaScript -->
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/js/materialize.min.js"></script>
       @yield('extra-js')
+      <script type="text/javascript">
+        $(document).ready(function(){
+          $(".dropdown-button").dropdown();
+          $(".button-collapse").sideNav();
+        })
+      </script>
 </body>
 </html>

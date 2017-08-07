3651bff21782978957f49ac0567ba379cb3f7e31
gil <blackdem17@hotmail.com>
2017-08-07 18:49:24 -0500

    :clipboard: login view changed, register view was moved to the index page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,58 +1,112 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container blue lighten-5 z-depth-3" id="main" style="margin-padding:10px;">
-  <!--h5 class="indigo center-align"><font color="white">Inicia sesión</font></h5-->
-  <div class="container border-radius: 10px">
-    <div class="row">
-      <div class="col s9 offset s3">
-        <!--Materialize Components Forms-->
-        <div class="row">
-          <form class="col s12" action="{{route('login')}}" method="POST">
+  <div class="card">
+    <div class="card-content">
+      <h3 class="">Bienvenido</h3>
+    </div>
+    <div class="card-tabs">
+      <ul class="tabs tabs-fixed-width">
+        <li class="tab"><a href="#loginform" class="active blue-text">
+          <i class="material-icons">account_circle</i>
+          Entrar</a></li>
+        <li class="tab"><a  href="#registerform" class="blue-text">
+          <i class="material-icons">person_add</i>
+          Registrarse</a></li>
+      </ul>
+    </div>
+    <div class="card-content grey lighten-4">
+      <div id="loginform">
+        <form class="col s12" action="{{route('login')}}" method="POST">
+          {{ csrf_field() }}
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="email" type="email" class="validate" name="email">
+              <label for="email" data-error="wrong" data-success="right">
+                <i class="material-icons">email</i>
+                Email</label>
+              <!--error for email-->
+              @if ($errors->has('email'))
+                  <span class="help-block">
+                      <strong>{{ $errors->first('email') }}</strong>
+                  </span>
+              @endif
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="input-field col s12" >
+              <input id="password" type="password" class="validate" name="password">
+              <label for="password">
+                <i class="material-icons">vpn_key</i>
+                Password</label>
+              <!--errors for password-->
+              @if ($errors->has('password'))
+                  <span class="help-block">
+                      <strong>{{ $errors->first('password') }}</strong>
+                  </span>
+              @endif
+            </div>
+          </div>
+          <div class="center-align">
+            <input type="submit" name="" value="Entrar" class="btn waves-effect waves-light z-depth-5">
+          </div>
+        </form>
+      </div><!--end login-->
+      <div id="registerform">
+        <form class="form-horizontal" method="POST" action="{{ route('register') }}">
             {{ csrf_field() }}
-            <div class="row">
-              <div class="input-field col s12">
-                <input id="email" type="email" class="validate" name="email">
-                <label for="email" data-error="wrong" data-success="right">
-                  <i class="fa fa-user-o"></i>
-                  Email</label>
-                <!--error for email-->
-                @if ($errors->has('email'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
-              </div>
+            <div class="input-field col s12">
+              <input placeholder="nombre" id="name" name="name" type="text" class="validate" value="{{ old('name') }}" required autofocus>
+              <label for="name">
+                <i class="material-icons">face</i>
+                Nombre
+              </label>
+              @if ($errors->has('name'))
+                  <span class="help-block">
+                      <strong>{{ $errors->first('name') }}</strong>
+                  </span>
+              @endif
             </div>
-
-            <div class="row">
-              <div class="input-field col s12" >
-                <input id="password" type="password" class="validate" name="password">
-                <label for="password">
-                  <i class="fa fa-key"></i>
-                  Password</label>
-                <!--errors for password-->
-                @if ($errors->has('password'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
-              </div>
+            <div class="input-field col s12">
+              <input placeholder="email" id="email" name="email" type="email" class="validate" value="{{ old('email') }}" required>
+              <label for="email">
+                <i class="material-icons">email</i>
+                Email
+              </label>
+              @if ($errors->has('email'))
+                  <span class="help-block">
+                      <strong>{{ $errors->first('email') }}</strong>
+                  </span>
+              @endif
             </div>
-
-            <div class="row">
-              <div class=""><a href="{{ route('password.request') }}">¿Olvidaste tu contraseña?</a></div>
+            <div class="input-field col s12">
+              <input placeholder="Contraseña" id="password" name="password" type="password" class="validate" value="{{ old('password') }}" required>
+              <label for="password">
+                <i class="material-icons">vpn_key</i>
+                Contraseña
+              </label>
+              @if ($errors->has('password'))
+                  <span class="help-block">
+                      <strong>{{ $errors->first('password') }}</strong>
+                  </span>
+              @endif
+            </div>
+            <div class="input-field col s12">
+              <input placeholder="" id="password-confirm" name="password_confirmation" type="password" class="validate" required>
+              <label for="first_name">
+                <i class="material-icons">vpn_key</i>
+                Confirma tu contraseña
+              </label>
             </div>
             <div class="center-align">
-              <input type="submit" name="" value="Entrar" class="btn waves-effect waves-light z-depth-5">
+              <button type="submit" class="btn btn-waves">Registrarse</button>
             </div>
-          </form>
-        </div>
+
+        </form>
       </div>
     </div>
   </div>
 
-  <hr>
 
-</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,75 +1,61 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <div class="row" style="margin-top:25px;">
+        <div class="col s12 m6 l8 xl8 m6 l8 xl8 offset-m3 offset-l2 offset-xl2">
+          <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+              {{ csrf_field() }}
+              <div class="input-field col s12">
+                <input placeholder="nombre" id="name" name="name" type="text" class="validate" value="{{ old('name') }}" required autofocus>
+                <label for="name">
+                  <i class="material-icons">face</i>
+                  Nombre
+                </label>
+                @if ($errors->has('name'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('name') }}</strong>
+                    </span>
+                @endif
+              </div>
+              <div class="input-field col s12">
+                <input placeholder="email" id="email" name="email" type="email" class="validate" value="{{ old('email') }}" required>
+                <label for="email">
+                  <i class="material-icons">email</i>
+                  Email
+                </label>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+              </div>
+              <div class="input-field col s12">
+                <input placeholder="Contraseña" id="password" name="password" type="password" class="validate" value="{{ old('password') }}" required>
+                <label for="password">
+                  <i class="material-icons">vpn_key</i>
+                  Contraseña
+                </label>
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+              </div>
+              <div class="input-field col s12">
+                <input placeholder="" id="password-confirm" name="password_confirmation" type="password" class="validate" required>
+                <label for="first_name">
+                  <i class="material-icons">vpn_key</i>
+                  Confirma tu contraseña
+                </label>
+              </div>
+              <div class="input-field col s12">
+                <div class="col m6 offset-m3">
+                  <button type="submit" class="btn btn-waves">Registrarse</button>
                 </div>
-            </div>
+              </div>
+
+          </form>
         </div>
     </div>
 </div>

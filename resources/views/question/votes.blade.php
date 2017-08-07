333ac2d4a5af8315f3aa3c78d6e2408c7c8e8d08
gil <blackdem17@hotmail.com>
2017-08-07 18:53:53 -0500

    :clipboard: list to display votes added

--- resources/views/question/votes.blade.php
+++ resources/views/question/votes.blade.php
@@ -1,20 +1,80 @@
 @extends('layouts.master')
 
 @section('content')
-  <h1 class="text-center">{{$question->title}}</h1>
+  <div class="center-align">
+    <h1 >{{$question->title}}</h1>
+  </div>
   <input type="hidden" name="" value="{{$question->id}}" id="question_id">
 
-  <canvas id="votes" width="100px" height="100px"></canvas>
+  <div class="row" id="vote">
+    <div class="col s8">
+      <canvas id="votes" width="10px" height="10px"></canvas>
+    </div>
+    <div class="col s4">
+      <ul class="collection">
+        <li class="collection-item active">Resultados</li>
+        <li class="collection-item" v-for="answer in answers">
+          @{{answer.description}} <hr>
+
+          votos: @{{answer.votes}} <i class="material-icons">thumb_up</i>
+        </li>
+      </ul>
+    </div>
+  </div>
 @endsection
 
 @section('extra-js')
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js">
   </script>
+  <script type="text/javascript" src="https://unpkg.com/vue@2.4.1"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.2/axios.min.js"></script>
+
   <script type="text/javascript">
     var question_id = $("#question_id").val();
+    new Vue({
+      el: "#vote",
+      created: function(){
+        this.getQuestion();
+        console.log(this.answers)
+      },
+      data: {
+        answers: [],
+      },
+      methods:{
+        getQuestion: function(){
+          axios.get('http://localhost:8000/votesanswers/' + question_id).then(response => {
+            this.answers = response.data.answers;
+            var mylabels = [];
+            var totalVotes = [];
+            var colors = [
+              '#f44336',//0
+              '#2196f3',//1
+              '#00bcd4',//2
+              '#009688',//3
+              '#4caf50',//4
+              '#64ffda',//5
+              '#ff9800',//6
+              '#ff5722'//7
+            ];
+            $.each(this.answers,function(i, item){
+              mylabels.push(data.answers[i].description);
+              totalVotes.push(data.answers[i].votes);
+              var newNumber = getRandomInt();
+              console.log(newNumber);
+              graphsColors.push(colors[newNumber]);
 
+            });
+          }).catch(error => {
 
-      
+          });
+        },
+      }
+
+    });
+  </script>
+
+  <script type="text/javascript">
+    var question_id = $("#question_id").val();
       $.ajax({
         url : '/votesanswers/' + question_id,
         type : 'GET',
@@ -56,7 +116,7 @@
                     label: '# Votos',
                     data: totalVotes,
                     backgroundColor: graphsColors,
-                    borderColor: graphsColors.reverse(),
+                    borderColor: '#000000',
                     borderWidth: 2
                 }]
             },

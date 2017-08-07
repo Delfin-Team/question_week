e380678d3d04c8686b42f4e3a8aac8c53499e7e9
gil <blackdem17@hotmail.com>
2017-08-07 18:53:06 -0500

    :clipboard: navbar changed, new icons and items added

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,46 +1,49 @@
 <nav>
   @if (Auth::guest())
     <div class="nav-wrapper indigo accent-3">
-      <a href="index.html" class="brand-logo"> &nbsp Pregunta de la semana</a>
-      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="{{route('login')}}">Iniciar sesión</a></li>
-        <li><a href="{{route('register')}}">Registro</a></li>
-      </ul>
-      <ul class="side-nav" id="mobile-demo">
-        <li><a href="login.html">Iniciar sesión</a></li>
-        <li><a href="register.html">Registro</a></li>
-      </ul>
+      <a href="{{url('/')}}" class="brand-logo"> &nbsp Pregunta de la semana</a>
     </div>
   @else
 
 	    <div class="nav-wrapper indigo accent-3">
 	      <a href="{{route('groups.index')}}" class="brand-logo">&nbsp Pregunta de la Semana</a>
-	      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+	      <a href="#" data-activates="mobile-demo2" class="button-collapse"><i class="material-icons">menu</i></a>
 	      <ul id="nav-mobile" class="right hide-on-med-and-down">
-	        
+          <li><a class="dropdown-button" href="#!" data-activates="dropdown1">{{Auth::user()->email}}<i class="material-icons right">arrow_drop_down</i></a></li>
           <li>
-              <a href="{{ route('logout') }}"
-                  onclick="event.preventDefault();
-                           document.getElementById('logout-form').submit();">
-                  Logout
-              </a>
+            <ul id="dropdown1" class="dropdown-content">
+              <li><a href="{{url('/profile')}}">
+                <i class="material-icons">account_box</i>
+                Mi perfil</a></li>
+              <li class="divider"></li>
 
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                  {{ csrf_field() }}
-              </form>
+              <li>
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                    <i class="material-icons">power_settings_new</i>
+                    Salir
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+              </li>
+            </ul>
           </li>
 	      </ul>
-	      <ul class="side-nav" id="mobile-demo">
-	        <li><a href="propose.html"><i class="material-icons">lightbulb_outline</i>Proponer</a></li>
-	        <li><a href="winquestions.html"><i class="material-icons">timeline</i>Preguntas ganadoras</a></li>
-	        <li><a href="votes.html"><i class="material-icons">thumbs_up_down</i>Votos</a></li>
+
+	      <ul class="side-nav" id="mobile-demo2">
+          <li><a href="#" class="deactivate">Opciones</a></li>
+          <li class="divider"></li>
 	        <li><a href="{{route('groups.index')}}"><i class="material-icons">group</i>Grupos</a></li>
+          <li><a href="{{url('/profile')}}"><i class="material-icons">account_circle</i>Mi Perfil</a></li>
           <li>
               <a href="{{ route('logout') }}"
                   onclick="event.preventDefault();
                            document.getElementById('logout-form').submit();">
-                  Logout
+                  <i class="material-icons">power_settings_new</i>
+                  Salir
               </a>
 
               <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -52,11 +55,3 @@
 	    </div>
   @endif
 </nav>
-
-@section('extra-js')
-<script type="text/javascript">
-  $( document ).ready(function(){
-    $(".button-collapse").sideNav();
-  })
-</script>
-@endsection

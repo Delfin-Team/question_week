9a4a10c9df8e00297895d60b113861d8c9d6a601
gil <blackdem17@hotmail.com>
2017-08-08 00:10:14 -0500

    :clipboard: folder to support spanish language added to the project

--- app/User.php
+++ app/User.php
@@ -3,7 +3,7 @@
 namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use Illuminate\Auth\Passwords\CanResetPassword;
 class User extends Authenticatable
 {
     use Notifiable;
@@ -13,6 +13,7 @@ class User extends Authenticatable
      *
      * @var array
      */
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
